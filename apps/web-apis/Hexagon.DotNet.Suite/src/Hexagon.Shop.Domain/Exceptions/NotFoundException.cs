553c5d6ec8642c0c7973279f82f36f508f44b1b7
Alejandro Junqui <ajunquit@gmail.com>
2025-08-18 19:03:58 -0500

    add NotFoundException

---
+++ apps/web-apis/Hexagon.DotNet.Suite/src/Hexagon.Shop.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Hexagon.Shop.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string error) : base(error)
+        {
+
+        }
+
+        public NotFoundException(string error, Exception innerException) : base(error, innerException)
+        {
+        }
+    }
+}
